20e0798347044bac3878c62d356a17d6d46c6835
John mwanzia <johnmwanzia277@gmail.com>
2023-07-09 13:01:03 +0300

    add create update handler

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -24,6 +24,23 @@ const  updates = products.reduce((allUpdates, product) => {
     res.json({ data: updates });
 };
    
-export const createUpdate = async (req, res) => {};
+export const createUpdate = async (req, res) => {
+    const product = await prisma.product.findFirst({
+        where: {
+            id: req.body.productId,
+        },
+    });
+
+    if (!product) {
+        res.status(400).json({ error: "Product not found" });
+        return;
+    }
+
+    const update = await prisma.update.create({
+        data: req.body,
+    });
+
+    res.json({ data: update });
+};
 export const updatedUpdate = async (req, res) => {};
 export const deleteUpdate = async (req, res) => {};
