a7061c40fbba2ff70b50c7435902b79db99b0aaf
John mwanzia <johnmwanzia277@gmail.com>
2023-07-10 11:48:10 +0300

    create an error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,8 @@ app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // allows a client to add things like query string and params and it encodes and decodes it correctly for us
 
 app.get('/', (req, res) => {
-    res.json({message: 'Hello from server'});
+    // res.json({message: 'Hello from server'});
+    throw new Error('Something went wrong');
 });
 
 app.use('/api',protect, router); // all routes will be prefixed with /api   // protect is a middleware that will run before the router
@@ -21,4 +22,10 @@ app.use('/api',protect, router); // all routes will be prefixed with /api   // p
 app.post('/user', createNewUser);
 app.post('/signin', signIn);
 
+//error handling middleware
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({error: 'Something went wrong'});
+});
 export default app;
